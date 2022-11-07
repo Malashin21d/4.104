c5bf7e84141b3aa4b80daf937d6400e0c2d1ccc8
Сергей <@ANDREYNOTEBOOK>
2022-11-07 20:50:03 +0300

    Добавьте файлы проекта.

---
+++ 4.104/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Введите номер дня недели");
+int day = int.Parse(Console.ReadLine());// программа по выводу названия дня недели.
+if (day < 1 || day > 7)
+{
+    Console.WriteLine("ОШИБКА!");
+}// Ошибка в случее не верного числа
+switch (day)
+{
+    case 1:
+        Console.WriteLine(week.ПОНЕДЕЛЬНИК);
+        break;
+    case 2:
+        Console.WriteLine(week.ВТОРНИК);
+        break;
+    case 3:
+        Console.WriteLine(week.СРЕДА);
+        break;
+    case 4:
+        Console.WriteLine(week.ЧЕТВЕРГ);
+        break;
+    case 5:
+        Console.WriteLine(week.ПЯТНИЦА);
+        break;
+    case 6:
+        Console.WriteLine(week.СУББОТА);
+        break;
+    case 7:
+        Console.WriteLine(week.ВОСКРЕСЕНЬЕ);
+        break;
+
+}// Дни недели в зависимости с числом.
+
+enum week
+{
+    ПОНЕДЕЛЬНИК,
+    ВТОРНИК,
+    СРЕДА,
+    ЧЕТВЕРГ,
+    ПЯТНИЦА,
+    СУББОТА,
+    ВОСКРЕСЕНЬЕ
+}// Ответ
